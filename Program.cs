b08638b5b13de7b390bb97f359dd381f2d5ae176
Monika Heang <mheang01@student.rtc.edu>
2023-09-26 15:05:32 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Code_Refresher
 {
     internal class Program
     {
-       
+
         static void Main(string[] args)
         {
             List<Player> players = new List<Player>();
@@ -29,24 +29,24 @@ namespace Code_Refresher
             players.Add(player5);
 
         Menu:;
-                Console.WriteLine($"\nPlayers" +
-                    $"\n1 - Display PLayers" +
-                    $"\n2 - Display PLayers with an Odd Number" +
-                    $"\n3 - Display PLayers starting with a letter" +
-                    $"\n4 - Add PLayers");
-                Console.Write($"Enter your choice: ");
-            
-           string userInput = Console.ReadLine();
-            switch (userInput) 
+            Console.WriteLine($"\nPlayers" +
+                $"\n1 - Display PLayers" +
+                $"\n2 - Display PLayers with an Odd Number" +
+                $"\n3 - Display PLayers starting with a letter" +
+                $"\n4 - Add PLayers");
+            Console.Write($"Enter your choice: ");
+
+            string userInput = Console.ReadLine();
+            switch (userInput)
             {
                 case "1":
-                    Preload();
+
                     for (int i = 0; i < players.Count; i++)
                     {
                         Console.WriteLine(players[i]);
                     }
                     Console.WriteLine($"-------------------------------\n\n\n");
-                    break; 
+                    break;
                 case "2":
                     DisplayOddNumber(players);
                     Console.WriteLine($"-------------------------------\n\n\n");
@@ -54,39 +54,39 @@ namespace Code_Refresher
                 case "3":
                     DisplayFirstLetterOfName(players);
                     Console.WriteLine($"-------------------------------\n\n\n");
-                    break; 
+                    break;
                 case "4":
                     AddPlayer(players);
                     Console.WriteLine($"-------------------------------\n\n\n");
                     break;
                 default:
-
+                    Console.WriteLine("Pick the right choice 1 to 4");
                     break;
-            
-            }goto Menu;
-            
+
+            } goto Menu;
+
         }
 
         public static void DisplayOddNumber(List<Player> players)
         {
-            for(int i =0; i < players.Count; i++)
+            for (int i = 0; i < players.Count; i++)
             {
-                if (players[i].Number %2 != 0)
+                if (players[i].Number % 2 != 0)
                 {
                     Console.WriteLine(players[i]);
                 }
             }
         }
 
-        public static void DisplayFirstLetterOfName(List<Player> players) 
+        public static void DisplayFirstLetterOfName(List<Player> players)
         {
             Console.Write($"Enter the letter: ");
             string enter = Console.ReadLine();
             int i;
-         
-            for (i = 0; i < players.Count; i++) 
+
+            for (i = 0; i < players.Count; i++)
             {
-               string playerName = players[i].Name[0].ToString();
+                string playerName = players[i].Name[0].ToString();
 
                 if (enter == playerName)
                 {
@@ -98,35 +98,38 @@ namespace Code_Refresher
                     Console.WriteLine(players[i]);
                 }
             }
-        }
 
-        public static void AddPlayer(List<Player> player) 
+            if (enter == enter.ToLower())
+            {
+                Console.WriteLine($"No Player's Name start with {enter}");
+
+            }
+            else 
+            {
+                Console.WriteLine($"No Player's Name start with {enter}");
+            }
+        }
+           
+
+        public static void AddPlayer(List<Player> player)
         {
             Console.Write($"Enter Name: ");
             string name = Console.ReadLine();
 
             Console.Write($"Enter Number: ");
             string number = Console.ReadLine();
-            int convertNum = int.Parse( number );
+            bool isNumber = int.TryParse(number, out int Num);
 
-            player.Add(new Player(name, convertNum));
+            if (isNumber)
+            {
+                player.Add(new Player(name, Num));
+            }
+
+            else
+            {
+                Console.WriteLine("Enter Number Value");
+            }
 
         }
-
-
-        public static void Preload()
-        {
-            //Player player1 = new Player("Billy", 12);
-            //Player player2 = new Player("Victor", 33);
-            //Player player3 = new Player("Sally", 67);
-            //Player player4 = new Player("Janell", 45);
-            //Player player5 = new Player("Hudson", 3);
-
-            //players.Add(player1);
-            //players.Add(player2);
-            //players.Add(player3);
-            //players.Add(player4);
-            //players.Add(player5);
-        }
-    }
-}
+    }//class 
+}//namespace
